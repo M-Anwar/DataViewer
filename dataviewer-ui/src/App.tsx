8c3f9b3933575f83e02e42c84c3bcc205b481f87
Muhammed Anwar <muhammed.anwar@xero.com>
2026-02-15 13:15:02 -0500

    Switching to primereact ui

--- dataviewer-ui/src/App.tsx
+++ dataviewer-ui/src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from 'primereact/button';
 
 function App() {
   return (
     <div className="min-h-screen grid place-items-center text-center">
       <div className="space-y-4">
         <h1 className="text-3xl font-semibold">Hello world</h1>
-        <Button color="green">Click me</Button>
+        <Button label="Click me" severity="success" />
       </div>
     </div>
   )
